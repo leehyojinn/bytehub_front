372af941a1ff4b84b489de7659718aacb20280a4
sang_yoon <dltkddbsfl@gmail.com>
2025-07-07 12:44:55 +0900

    일단 커밋

--- src/app/component/layout.jsx
+++ src/app/component/layout.jsx
@@ -2,19 +2,27 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useAlertModalStore } from "@/app/zustand/store";
 
 export default function Layout({ children }) {
     const router = useRouter();
     const [isAuthChecked, setIsAuthChecked] = useState(false);
+    const alertModal = useAlertModalStore();
 
     useEffect(() => {
         if (!sessionStorage.getItem('userId') || !sessionStorage.getItem('token')) {
-            alert('로그인이 필요합니다.');
-            router.replace('/');
+
+            alertModal.openModal({
+                svg: "❗",
+                msg1: "로그인이 필요합니다.",
+                msg2: "로그인 후 이용해 주세요.",
+                showCancel: false,
+                onConfirm: () => window.location.replace('http://localhost/')
+            });
         } else {
             setIsAuthChecked(true);
         }
-    }, [router]);
+    }, [router, alertModal]);
 
     if (!isAuthChecked) return null;
 
